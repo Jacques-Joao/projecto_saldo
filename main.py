2ddb156558017805941cc8eeb1183a3358ae73d2
Jacques João <61160325+Jacques-Joao@users.noreply.github.com>
2020-05-23 00:01:34 +0100

    Interface básica feita!!

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from kivy.uix.label import Label
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from random import randint
+
+
+class Interface(FloatLayout, Widget):
+
+    def clicado(self, **kwargs):
+        self.num = randint(100000000000, 999999999999)
+        return print(f'Fui clicado! E o número gerado foi {self.num}')
+
+
 
 class UttsApp(App):
 
     def build(self):
-        return Label(text="First Steps")
+        self.icon = 'pngwing.com.png'
+        return Interface()
+
 
 if __name__ == '__main__':
     UttsApp().run()
-
