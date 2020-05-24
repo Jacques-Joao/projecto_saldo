616420c6d0ccf9dc24b544c5455c247aa50956e6
Jacques João <61160325+jacques-joao@users.noreply.github.com>
2020-05-24 18:02:09 +0100

    Interface básica feita!!

--- main.py
+++ main.py
@@ -1,14 +1,57 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
 from random import randint
+from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
 
 
-class Interface(FloatLayout, Widget):
+class Caixa(CheckBox):
 
-    def clicado(self, **kwargs):
+    def caixa_unitel(self, instance, value):
+        if value is True:
+            print("Unitel Checkbox Checked")
+        else:
+            print("Unitel Checkbox Unchecked")
+
+    def caixa_movicel(self, instance, value):
+        if value is True:
+            print("Movicel Checkbox Checked")
+        else:
+            print("Movicel Checkbox Unchecked")
+
+
+class Btn(Widget):
+
+    def clicado(self):
         self.num = randint(100000000000, 999999999999)
-        return print(f'Fui clicado! E o número gerado foi {self.num}')
+        face = Interface()
+        face.iniciar(self.num)
+        self.bora = face.iniciar
+        # del face.iniciar
+        return self.bora
+
+
+class Interface(FloatLayout):
+    btn = Btn()
+    cxa = Caixa()
+
+    def iniciar(self, *algo):
+        self.continuar = True
+        # while self.continuar:
+        self.inicio = algo[0]
+        print(f'Fui clicado! E o número gerado foi {self.inicio}')
+        # Clock
+        # print(Clock)
+
+    def parar(self):
+        print('Parar')
+        self.continuar = False
+
+    def checkbox_click(self, instance, value):
+        if value is True:
+            print("Checkbox Checked")
+        else:
+            print("Checkbox Unchecked")
 
 
 
