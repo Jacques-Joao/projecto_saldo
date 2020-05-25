487c18aef5fd110edbe489839baf1535441116e4
Jacques João <61160325+jacques-joao@users.noreply.github.com>
2020-05-25 02:53:27 +0100

    - Funcionalidades melhoradas dos botões e checkbox's! - Melhora na atribuição de valores ao checkbox's

--- main.py
+++ main.py
@@ -22,25 +22,35 @@ class Btn(Widget):
             print("Movicel Checkbox Unchecked")
         return self.value_movicel
 
-    def clicado(self, unitel=False, movicel=False):
-        if self.value_unitel == self.value_movicel == True or self.value_unitel == self.value_movicel == False:
-            print('Somente uma das caixas de marcação deve ser selecionada')
+    def clicado(self):
+        self.codigo = ''
+        try:
+            if self.value_unitel == self.value_movicel == True:
+                print('Desmarque uma das redes.')
+        except:
+            print('Por favor, selecione apenas uma rede!')
+            pass
+        try:
+            if self.value_unitel == self.value_movicel == False:
+                print('Somente uma das caixas de marcação deve ser selecionada')
+        except:
+            print('Antes de inicar o programa, marque as 2 caixas e em seguida desmarque a rede não desejada!')
+            pass
         else:
             if self.value_unitel and not self.value_movicel:
                 print('Operadora Unitel selecioanado!')
+                self.codigo = str('*100*')
+                return self.codigo
             elif self.value_movicel and not self.value_unitel:
                 print('Operadora Movicel Selecionada')
+                self.codigo = str('*196*')
+                return self.codigo
+        finally:
             self.num = randint(100000000000, 999999999999)
             face = Interface()
-            face.iniciar(self.num)
+            face.iniciar(saldo=self.num, rede=self.codigo)
             self.bora = face.iniciar
             return self.bora
-
-    def movicel(self):
-        pass
-
-    def unitel(self):
-        pass
 
     def validar(self):
         valores = Btn()
@@ -53,9 +63,11 @@ class Btn(Widget):
 class Interface(FloatLayout):
     btn = Btn()
 
-    def iniciar(self, *algo):
-        self.inicio = algo[0]
-        print(f'Fui clicado! E o número gerado foi {self.inicio}')
+    def iniciar(self, saldo, rede):
+        self.saldo = saldo
+        self.rede = rede
+        print(f'Fui clicado! E o número gerado foi {self.saldo}')
+        print(f'{self.rede}{self.saldo[0]}#')
 
     def parar(self):
         print('Parar')
